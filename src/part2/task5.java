269af6ce9189225876e3f6df290118929bd303f1
Natali3004 <n.lizun3004@gmail.com>
2023-03-23 12:10:16 +0300

    task 5 done

---
+++ src/part2/task5.java
@@ -0,0 +1,15 @@
+package part2;
+
+public class task5 {
+    public static void main(String[] args) {
+        System.out.println(run());
+    }
+
+    public static int run() {
+        int x = 5;
+        int y = 7;
+        System.out.println(x);
+        System.out.println(y);
+        return x + y + x * y;
+    }
+}

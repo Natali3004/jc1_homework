130e04e7a4d38ba46ebf13d6d206facf1e27c12e
Natali3004 <n.lizun3004@gmail.com>
2023-03-28 15:02:33 +0300

    task 3 done

---
+++ src/part2/task3.java
@@ -0,0 +1,8 @@
+package part2;
+
+public class task3 {
+    public static void main(String[] args) {
+        System.out.println("Hello Belarus!");
+        System.out.println("I like Java!");
+    }
+}

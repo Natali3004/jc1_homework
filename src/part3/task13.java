85b55d53c3c39a2b7144fe200dd265d032d132ee
Natali3004 <n.lizun3004@gmail.com>
2023-03-29 12:52:19 +0300

    task 13 done

---
+++ src/part3/task13.java
@@ -0,0 +1,14 @@
+package part3;
+
+public class task13 {
+    public static void main(String[] args) {
+        int x = 1;
+        int y = 10;
+        int result = 1;
+        while (x <= y) {
+            result = result * x;
+            System.out.println("Факториал " + x + "  равен = " + result);
+            x++;
+        }
+    }
+}

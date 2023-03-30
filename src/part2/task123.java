a3668a1d056a9f1b5f511f8811c1912ebea94e0c
Natalia <127677766+Natali3004@users.noreply.github.com>
2023-03-30 18:35:43 +0300

    Delete task123.java

--- src/part2/task123.java
+++
@@ -1,8 +0,0 @@
-package part2;
-
-public class task123 {
-    public static void main(String[] args) {
-        System.out.println("Hello Belarus!");
-        System.out.println("I like Java!");
-    }
-}

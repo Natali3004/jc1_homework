b1b41e9cc9d2ddd6797d72289317f3fb6bcd75b7
Natali3004 <n.lizun3004@gmail.com>
2023-04-01 14:47:52 +0300

    task 3 renamed

--- src/part2/Task3.java
+++ src/part2/Task3.java
@@ -1,6 +1,6 @@
 package part2;
 
-public class task3 {
+public class Task3 {
     public static void main(String[] args) {
         System.out.println("Hello Belarus!");
         System.out.println("I like Java!");

4d7e1fddd7233380f17e16894576c778beb063c9
Natali3004 <n.lizun3004@gmail.com>
2023-04-01 14:05:44 +0300

    task 19 done

---
+++ src/part3/Task19.java
@@ -0,0 +1,9 @@
+package part3;
+
+public class Task19 {
+    public static void main(String[] args) {
+
+        int[] myMasiv = {5, 2, 3, 6, 9, 7, 12, 15, 0, 1};
+        System.out.println("Вывод последнего элемента массива myMasiv: " + myMasiv[9]);
+    }
+}

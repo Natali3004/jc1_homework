512fc2763f790de281d284b40b45a8e45d300f6a
Natali3004 <n.lizun3004@gmail.com>
2023-04-02 22:41:27 +0300

    task 23 changed

--- src/part3/Task23.java
+++ src/part3/Task23.java
@@ -3,19 +3,20 @@ package part3;
 public class Task23 {
     public static void main(String[] args) {
         int[] myMasiv = {5, 0, 3, 6, 9, 7, 12, 15, 8, 1};
-        System.out.println("Первоначальный массив: " + myMasiv[0] + ", " + myMasiv[1] + ", " + myMasiv[2] + ", "
-                + myMasiv[3] + ", " + myMasiv[4] + ", " + myMasiv[5] + ", " + myMasiv[6] + ", " + myMasiv[7] + ", " + myMasiv[8] +
-                ", " + myMasiv[9]);
+        System.out.print("Первоначальный массив: ");
+        for (int i = 0; i < myMasiv.length; i++) {
+            System.out.print(myMasiv[i] + " , ");
+        }
 
         for (int i = 0; i < myMasiv.length / 2; i++) {
             int temp = myMasiv[i];
             myMasiv[i] = myMasiv[myMasiv.length - i - 1];
             myMasiv[myMasiv.length - i - 1] = temp;
         }
-        System.out.println("Перевернутый массив: " + myMasiv[0] + ", " + myMasiv[1] + ", " + myMasiv[2] + ", "
-                + myMasiv[3] + ", " + myMasiv[4] + ", " + myMasiv[5] + ", " + myMasiv[6] + ", " + myMasiv[7] + ", " + myMasiv[8] +
-                ", " + myMasiv[9]);
+        System.out.print("\nПеревернутый массив: ");
+        for (int i = 0; i < myMasiv.length; i++) {
+            System.out.print(myMasiv[i] + " , ");
 
+        }
     }
 }
-

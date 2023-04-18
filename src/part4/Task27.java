781cc02e546bd1fd958c21e5cb4f5e8e894e58cd
Natali3004 <n.lizun3004@gmail.com>
2023-04-18 14:04:10 +0300

    task 27 done

---
+++ src/part4/Task27.java
@@ -0,0 +1,19 @@
+package part4;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Task27 {
+    public static void main(String[] args) {
+        String str = " !!! Часть регулярного выражения - можно выделить в группу, для этого его следует взять в () круглые скобки.";
+        Pattern pattern = Pattern.compile("[\\p{P}]");
+        Matcher matcher = pattern.matcher(str);
+        int count = 0;
+
+        while (matcher.find()) {
+            ++count;
+        }
+        System.out.println("Количество знаков препинания: " + count);
+    }
+}
+

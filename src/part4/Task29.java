257751eac87d5645bd0c3b83ea3f878ab7b3befb
Natali3004 <n.lizun3004@gmail.com>
2023-04-18 14:05:58 +0300

    task 29 done

---
+++ src/part4/Task29.java
@@ -0,0 +1,15 @@
+package part4;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Task29 {
+    public static void main(String[] args) {
+        String str = " !!! Часть регулярного выражения - можно выделить в группу, для этого его следует взять в () круглые скобки.";
+        Pattern pattern = Pattern.compile("([a-zа-я])\\b");
+        Matcher matcher = pattern.matcher(str);
+        while (matcher.find())
+            System.out.print(matcher.group(1));
+
+    }
+}

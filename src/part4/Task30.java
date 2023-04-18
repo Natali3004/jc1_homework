73a837ad1266a8b7f83bef5b31cab6a7e2d8231f
Natali3004 <n.lizun3004@gmail.com>
2023-04-18 14:06:19 +0300

    task 30 done

---
+++ src/part4/Task30.java
@@ -0,0 +1,23 @@
+package part4;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Task30 {
+
+    public static void searchNum() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите текст с шестнадцатиричными числами: ");
+        String name = in.nextLine();
+
+        Pattern pattern = Pattern.compile("\\b0?[xX]?[0-9a-fA-F]+\\b");
+        Matcher matcher = pattern.matcher(name);
+        int count = 0;
+        while (matcher.find()) {
+            ++count;
+        }
+        System.out.println("Количество шестнадцатиричных чисел в тексте: " + count);
+
+    }
+}
---
+++ src/part4/Task30Main.java
@@ -0,0 +1,10 @@
+package part4;
+
+import static part4.Task30.searchNum;
+
+public class Task30Main {
+    public static void main(String[] args) {
+        searchNum();
+
+    }
+}

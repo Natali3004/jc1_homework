af68a646a01a26f369515c6dade50234cae2fe35
Natali3004 <n.lizun3004@gmail.com>
2023-04-23 13:29:34 +0300

    task 37 done

---
+++ src/task37/Main.java
@@ -0,0 +1,12 @@
+package task37;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Введите параметр для вывода следующего сезона. 1 - весна; 2 - лето; 3 - осень; 4 - зима");
+
+        SeasonsOfYear.nextSeason();
+    }
+
+}
+

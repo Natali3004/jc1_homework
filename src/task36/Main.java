43382980dd66fffabdf85c1a319c55328f898e28
Natali3004 <n.lizun3004@gmail.com>
2023-04-23 13:27:40 +0300

    task 36 done

---
+++ src/task36/Main.java
@@ -0,0 +1,13 @@
+package task36;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        SeasonsOfYear myArray[] = SeasonsOfYear.values();
+        for (SeasonsOfYear a  : myArray)
+            System.out.println(a);
+
+    }
+
+}

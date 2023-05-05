5179945220e0c500a710ab3c88a1e2c8e53fe83e
Natali3004 <n.lizun3004@gmail.com>
2023-05-05 13:30:37 +0300

    task 40 done

--- src/part7/Task40.java
+++ src/part7/Task40.java
@@ -5,21 +5,23 @@ import java.util.Iterator;
 
 public class Task40 {
     public static void main(String[] args) {
+
         ArrayList<Integer> list = new ArrayList<>();
-        int value = 20;
-        int min = 3;
-        for (int i = 0; i < value; i++) {
+        for (int i = 0; i < 20; i++) {
             int mark = 1 + (int) (Math.random() * 10);
             list.add(mark);
         }
         System.out.println("All class marks: " + list);
 
-
-        for (Iterator<Integer> iterator = list.iterator();
-             iterator.hasNext(); )
-            if (iterator.next() <= min) {
-                iterator.remove();
+        ArrayList<Integer> listMin = new ArrayList<>();
+        int min = 3;
+        for (int i = 0; i < 20; i++) {
+            int num = list.get(i);
+            if (num <= min) {
+                listMin.add(num);
             }
+        }
+        list.removeAll(listMin);
         System.out.println("Only positive marks: " + list);
     }
 }

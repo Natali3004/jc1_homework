74e10a66cc47fa029d76f97a0e607371485a421c
Natali3004 <n.lizun3004@gmail.com>
2023-05-13 12:44:41 +0300

    task 45 done

---
+++ src/part8/task45/AgeException.java
@@ -0,0 +1,16 @@
+package part8.task45;
+class AgeException extends Exception {
+
+    private int age;
+    public AgeException(String message, int age) {
+        super(message);
+        this.age = age;
+    }
+}
+class Age {
+    public static int retirementAge(int age) throws AgeException {
+
+        if (age < 60 ) throw new AgeException("NO pension", age);
+        return age;
+    }
+}

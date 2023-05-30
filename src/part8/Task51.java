822f1ec8d7dd35fb98f3aca70d09dfcae3ea54c6
Natali3004 <n.lizun3004@gmail.com>
2023-05-30 13:45:23 +0300

    task 51 changed

--- src/part8/Task51.java
+++ src/part8/Task51.java
@@ -1,52 +1,38 @@
 package part8;
 
 import java.io.*;
+import java.util.Objects;
 import java.util.Random;
 
 public class Task51 {
     public static void main(String[] args) throws IOException {
 
-        File directory = new File("D:\\1\\2\\3");
+        File directory = new File("D:\\2\\2\\3");
         directory.mkdirs();
 
-        FileOutputStream oneDoc, oneDoc2, oneDoc3, oneDoc4, oneDoc5;
-        try {
-            oneDoc = new FileOutputStream("D:\\1\\2\\3\\one.txt" );
-            oneDoc2 = new FileOutputStream("D:\\1\\2\\3\\two.txt" );
-            oneDoc3 = new FileOutputStream("D:\\1\\2\\3\\three.txt" );
-            oneDoc4 = new FileOutputStream("D:\\1\\2\\3\\four.txt" );
-            oneDoc5 = new FileOutputStream("D:\\1\\2\\3\\five.txt" );
+        for (int i = 1; i <= 5; i++) {
+            File doc = new File(directory, "Doc_" + i + ".txt");
+            doc.createNewFile();
 
-            Random random = new Random();
+            try (OutputStream os = new FileOutputStream(doc)) {
+                Random random = new Random();
+                int[] array = new int[20];
+                for (int j = 0; j < array.length; ++j) array[j] = random.nextInt();
+                for (int j : array) os.write(String.valueOf(j).getBytes());
+            }
 
-            int[] array = new int[20];
-            for (int i = 0; i < array.length; ++i) array[i] = random.nextInt();
-
-            int[] array2 = new int[20];
-            for (int i = 0; i < array.length; ++i) array2[i] = random.nextInt();
-
-            int[] array3 = new int[20];
-            for (int i = 0; i < array.length; ++i) array3[i] = random.nextInt();
-
-            int[] array4 = new int[20];
-            for (int i = 0; i < array.length; ++i) array4[i] = random.nextInt();
-
-            int[] array5 = new int[20];
-            for (int i = 0; i < array.length; ++i) array5[i] = random.nextInt();
-
-            for (int j : array) oneDoc.write(String.valueOf(j).getBytes());
-            for (int j : array2) oneDoc2.write(String.valueOf(j).getBytes());
-            for (int j : array3) oneDoc3.write(String.valueOf(j).getBytes());
-            for (int j : array4) oneDoc4.write(String.valueOf(j).getBytes());
-            for (int j : array5) oneDoc5.write(String.valueOf(j).getBytes());
-            oneDoc.close();
-            oneDoc2.close();
-            oneDoc3.close();
-            oneDoc4.close();
-            oneDoc5.close();
-
-        } catch (IOException e) {
-            e.printStackTrace();
+            try (FileOutputStream allFiles = new FileOutputStream("D:\\2\\2\\3\\allFiles.txt")) {
+                try (FileInputStream reader = new FileInputStream(doc)) {
+                    allFiles.write(reader.readAllBytes());
+                }
+                try (FileOutputStream listOfFiles = new FileOutputStream("D:\\2\\2\\3\\ListOfFiles.txt")) {
+                    File myCatalog = new File("D:\\2\\2\\3");
+                    if (myCatalog.isDirectory()) {
+                        for (File num : Objects.requireNonNull(myCatalog.listFiles()))
+                            listOfFiles.write(num.getName().getBytes());
+                    }
+                }
+            }
         }
     }
 }
